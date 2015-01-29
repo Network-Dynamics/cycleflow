f9da73d44cee4fdf3ba6827c40b457c3950fcd71
Debsankha Manik <dmanik@estelli>
2015-01-29 17:13:58 +0100

    Don't use sparse.linalg.inv as clusters at Faßberg don't have it

--- cycleflows.py
+++ cycleflows.py
@@ -2,9 +2,38 @@ import networkx as nx
 import scipy.sparse as sparse 
 import scipy.sparse.linalg as spalg
 
+def spinv(A):
+	"""
+	Compute the inverse of a sparse matrix
+
+	.. versionadded:: 0.12.0
+
+	Parameters
+	----------
+	A : (M,M) ndarray or sparse matrix
+	    square matrix to be inverted
+
+	Returns
+	-------
+	Ainv : (M,M) ndarray or sparse matrix
+	    inverse of `A`
+
+	Notes
+	-----
+	This computes the sparse inverse of `A`.  If the inverse of `A` is expected
+	to be non-sparse, it will likely be faster to convert `A` to dense and use
+	scipy.linalg.inv.
+
+	"""
+	I = spalg.speye(A.shape[0], A.shape[1], dtype=A.dtype, format=A.format)
+	Ainv = spalg.spsolve(A, I)
+	return Ainv
+
+
+
 def dF_with_distance(G, src_a=None, src_b=None):
 	allnodes=G.nodes()
-	Lg=spalg.inv(sparse.csc_matrix(G.loopy_laplacian()))
+	Lg=spinv(sparse.csc_matrix(G.loopy_laplacian()))
 	
 	if src_b:
 		return [(nx.shortest_path_length(G, source=src_a, target=node) , Lg[allnodes.index(node), allnodes.index(src_a)] -Lg[allnodes.index(node), allnodes.index(src_b)]) for node in allnodes]
