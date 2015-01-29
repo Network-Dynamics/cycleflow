b5c1378c95ba76721c3251c6ef6fa3ed7c5dbb2f
Debsankha Manik <dmanik@estelli>
2015-01-29 19:57:30 +0100

    Added a lot of crap because scipy version in clusters is too old

--- cycleflows.py
+++ cycleflows.py
@@ -1,7 +1,90 @@
+from warnings import warn
 import networkx as nx
+
+from numpy import asarray, empty, where, squeeze, prod
 import scipy.sparse as sparse 
-import scipy.sparse.linalg as spalg
 from scipy.sparse.construct import eye as speye
+from scipy.sparse import isspmatrix_csc, isspmatrix_csr, isspmatrix, \
+        SparseEfficiencyWarning, csc_matrix
+
+from scipy.sparse.linalg import factorized
+
+def spsolve(A, b, permc_spec=None, use_umfpack=False):
+	"""Solve the sparse linear system Ax=b, where b is a matrix.
+
+	Parameters
+	----------
+	A : ndarray or sparse matrix
+		The square matrix A will be converted into CSC or CSR form
+	b : ndarray or sparse matrix
+		The matrix or vector representing the right hand side of the equation.
+		If a vector, b.size must
+	permc_spec : str, optional
+		How to permute the columns of the matrix for sparsity preservation.
+		(default: 'COLAMD')
+
+		- ``NATURAL``: natural ordering.
+		- ``MMD_ATA``: minimum degree ordering on the structure of A^T A.
+		- ``MMD_AT_PLUS_A``: minimum degree ordering on the structure of A^T+A.
+		- ``COLAMD``: approximate minimum degree column ordering
+	use_umfpack : bool (optional)
+		if True (default) then use umfpack for the solution.  This is
+		only referenced if b is a vector.
+
+	Returns
+	-------
+	x : ndarray or sparse matrix
+		the solution of the sparse linear equation.
+		If b is a vector, then x is a vector of size A.shape[1]
+		If b is a matrix, then x is a matrix of size (A.shape[1], b.shape[1])
+
+	Notes
+	-----
+	For solving the matrix expression AX = B, this solver assumes the resulting
+	matrix X is sparse, as is often the case for very sparse inputs.  If the
+	resulting X is dense, the construction of this sparse result will be
+	relatively expensive.  In that case, consider converting A to a dense
+	matrix and using scipy.linalg.solve or its variants.
+	"""
+	if not (isspmatrix_csc(A) or isspmatrix_csr(A)):
+		A = csc_matrix(A)
+		warn('spsolve requires A be CSC or CSR matrix format', SparseEfficiencyWarning)
+
+	# b.size gives a different answer for dense vs sparse:
+	# use prod(b.shape)
+
+	if isspmatrix(b) and not (isspmatrix_csc(b) or isspmatrix_csr(b)):
+		b = csc_matrix(b)
+		warn('solve requires b be CSC or CSR matrix format',
+			 SparseEfficiencyWarning)
+	if b.ndim != 2:
+		raise ValueError("b must be either a vector or a matrix")
+
+	A.sort_indices()
+	A = A.asfptype()  # upcast to a floating point format
+
+	# validate input shapes
+	M, N = A.shape
+	if (M != N):
+		raise ValueError("matrix must be square (has shape %s)" % ((M, N),))
+
+	if M != b.shape[0]:
+		raise ValueError("matrix - rhs dimension mismatch (%s - %s)"
+						 % (A.shape, b.shape[0]))
+
+
+	Afactsolve = factorized(A)
+	tempj = empty(M, dtype=int)
+	x = A.__class__(b.shape)
+	for j in range(b.shape[1]):
+		xj = Afactsolve(squeeze(b[:, j].toarray()))
+		w = where(xj != 0.0)[0]
+		tempj.fill(j)
+		x = x + A.__class__((xj[w], (w, tempj[:len(w)])),
+							shape=b.shape, dtype=A.dtype)
+	return x
+
+
 
 def spinv(A):
 	"""
@@ -27,7 +110,7 @@ def spinv(A):
 
 	"""
 	I = speye(A.shape[0], A.shape[1], dtype=A.dtype, format=A.format)
-	Ainv = spalg.spsolve(A, I)
+	Ainv = spsolve(A, I)
 	return Ainv
 
 
