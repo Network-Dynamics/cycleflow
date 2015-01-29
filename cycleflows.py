0551f1282bc0a9e83d0ec4b74689d9bd5a39a37f
Debsankha Manik <dmanik@estelli>
2015-01-29 15:58:29 +0100

    Using sparse matrices and scipy.sparse.linalg instead of numpy.linalg Speedup at least 10 fold

--- cycleflows.py
+++ cycleflows.py
@@ -1,9 +1,11 @@
 import numpy as np
 import networkx as nx
+import scipy.sparse as sparse 
+import scipy.sparse.linalg. as spalg
 
 def dF_with_distance(G, src_a=None, src_b=None):
 	allnodes=G.nodes()
-	Lg=np.linalg.pinv(G.loopy_laplacian())
+	Lg=spalg.inv(sparse.csc_matrix(G.loopy_laplacian()))
 	
 	if src_b:
 		return [(nx.shortest_path_length(G, source=src_a, target=node) , Lg[allnodes.index(node), allnodes.index(src_a)] -Lg[allnodes.index(node), allnodes.index(src_b)]) for node in allnodes]
