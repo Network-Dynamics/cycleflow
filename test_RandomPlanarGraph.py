8df567b6028e21b3cb63b93c3d24f1569c171e53
Debsankha Manik <dmanik@estelli>
2015-02-10 18:09:54 +0100

    started writing test cases

--- test_RandomPlanarGraph.py
+++ test_RandomPlanarGraph.py
@@ -1,1 +1,9 @@
 import unittest
+import RandomPlanarGraph
+class p_zero_TestCase(unittest.TestCase):
+	def setUp(self):
+		self.gridsize=10
+		G=RandomPlanarGraph.RandomPlanarGraph(self.gridsize,0)
+	
+	def test_graph_size(self):
+		assertTrue(G.number_of_nodes==self.gridsize**2 and G.number_of_edges==)
